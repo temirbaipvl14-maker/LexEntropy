0edf96d664da3b53b91b9f66e1deaa8ea65b9833
temirbaipvl14-maker <temirbaipvl14@gmail.com>
2026-04-02 23:11:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ except:
 st.set_page_config(page_title="LexEntropy | AI Анализ", layout="wide")
 st.markdown("""
 <style>
-    #MainMenu {visibility: hidden;} footer {visibility: hidden;} header {visibility: hidden;}
+    #MainMenu {visibility: hidden;} footer {visibility: hidden;} 
     .stButton>button[kind="primary"] {background-color: #5b45ff; color: white; border-radius: 8px;}
     .expert-box {background-color: #2e2b70; color: white; padding: 20px; border-radius: 12px; margin-top: 15px;}
 </style>
